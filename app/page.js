afd3df7a0b07755399f78e40f1fca21036f1a469
KOYAMA SHOYA <s1f102103189@iniad.org>
2024-03-09 16:54:10 +0900

    firebaseアプリの初期設定を追加

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import '../firebase'
 
 export default function Home() {
   // インラインスタイル
---
+++ firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD5YZOtJARI6xa6ACc3vLanLSNLQ6ymz8w",
+  authDomain: "shoya-diary.firebaseapp.com",
+  projectId: "shoya-diary",
+  storageBucket: "shoya-diary.appspot.com",
+  messagingSenderId: "542949924227",
+  appId: "1:542949924227:web:1ffdd84ce5b9f478d3aab6",
+  measurementId: "G-LFSSKM3YQL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+if(typeof window !== 'undefined'){
+    const analytics = getAnalytics(app);
+  }
